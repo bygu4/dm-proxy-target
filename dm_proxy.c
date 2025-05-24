cb0821ae9aae10697b4a08493b044099496e1870
bygu4 <sacha05bugaev@gmail.com>
2025-05-24 20:37:13 +0300

    add sysfs attribute for statistics displaying, update module initialization and exit

--- dm_proxy.c
+++ dm_proxy.c
@@ -6,12 +6,13 @@
  */
 
 #include <linux/device-mapper.h>
-
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/bio.h>
+#include <linux/sysfs.h>
 
 #define DM_MSG_PREFIX "proxy"
+#define KOBJ_NAME "stat"
 
 /*
  * A struct containing the block request statistics
@@ -22,53 +23,61 @@ struct bio_stats {
 	unsigned int req_count;
 };
 
+static struct bio_stats r_stats = {};
+static struct bio_stats w_stats = {};
+
+static struct kobject *stats_kobj;
+
 /*
- * A struct containing the proxy target context
+ * Display stats using the corresponding attribute
  */
-struct dm_proxy_target {
-	struct dm_dev *dev;
-	struct bio_stats *r_stats;
-	struct bio_stats *w_stats;
-};
+static ssize_t stats_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
+	size_t r_avg_block = r_stats.blk_count != 0 ? r_stats.total_size / r_stats.blk_count : 0;
+	size_t w_avg_block = w_stats.blk_count != 0 ? w_stats.total_size / w_stats.blk_count : 0;
+	size_t total_avg_block = r_stats.blk_count + w_stats.blk_count != 0
+		? (r_stats.total_size + w_stats.total_size) / (r_stats.blk_count + w_stats.blk_count)
+		: 0;
+	return sprintf(
+		buf,
+		"read:\n\treqs: %u\n\tavg_block: %zu\nwrite:\n\treqs: %u\n\tavg_block: %zu\ntotal:\n\treqs: %u\n\tavg_block: %zu\n",
+		r_stats.req_count,
+		r_avg_block,
+		w_stats.req_count,
+		w_avg_block,
+		r_stats.req_count + w_stats.req_count,
+		total_avg_block
+	);
+}
+
+static struct kobj_attribute stats_attr = __ATTR(volumes, 0444, stats_show, NULL);
 
 /*
  * Create a proxy target over the given device 
  */
-static int proxy_ctr(struct dm_target *ti, unsigned int argc, char **argv)
-{
-	struct dm_proxy_target *dmpt;
-
+static int proxy_ctr(struct dm_target *ti, unsigned int argc, char **argv) {
 	if (argc != 1) {
 		ti->error = "Invalid argument count";
 		return -EINVAL;
 	}
 
-	dmpt = (struct dm_proxy_target*)kmalloc(sizeof(struct dm_proxy_target), GFP_KERNEL);
+	struct dm_dev *dev;
 
-	if (dmpt == NULL) {
-		ti->error = "Failed to allocate proxy context";
-		return -ENOMEM;
-	}
-
-	if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &dmpt->dev)) {
+	if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &dev)) {
 		ti->error = "Device lookup failed";
-		kfree(dmpt);
 		return -EINVAL;
 	}
 
-	ti->private = dmpt;
+	ti->private = dev;
 	return 0;
 }
 
 /*
  * Release the proxy target context
  */
-static void proxy_dtr(struct dm_target *ti)
-{
-	struct dm_proxy_target *dmpt = ti->private;
+static void proxy_dtr(struct dm_target *ti) {
 
-	dm_put_device(ti, dmpt->dev);
-	kfree(dmpt);
+	struct dm_dev *dev = ti->private;
+	dm_put_device(ti, dev);
 }
 
 /*
@@ -90,30 +99,67 @@ static void update_stats(struct bio_stats *stats, struct bio *bio) {
  */
 static int proxy_map(struct dm_target *ti, struct bio *bio)
 {
-	struct dm_proxy_target *dmpt = ti->private;
+	struct dm_dev *dev = ti->private;
 
 	if (bio_data_dir(bio) == WRITE)
-		update_stats(dmpt->w_stats, bio);
+		update_stats(&w_stats, bio);
 	else
-		update_stats(dmpt->r_stats, bio);
+		update_stats(&r_stats, bio);
 
-	bio_set_dev(bio, dmpt->dev->bdev);
+	bio_set_dev(bio, dev->bdev);
 	submit_bio(bio);
 	return DM_MAPIO_SUBMITTED;
 }
 
-static struct target_type proxy_target = {
+static struct target_type dmpt = {
 	.name   = "proxy",
 	.version = {1, 0, 0},
-	.features = DM_TARGET_NOWAIT,
 	.module = THIS_MODULE,
 	.ctr    = proxy_ctr,
 	.dtr	= proxy_dtr,
 	.map    = proxy_map,
 };
-module_dm(proxy);
+
+/*
+ * Register the target and create sysfs on init
+ */
+static int __init proxy_init(void) {
+	stats_kobj = kobject_create_and_add(KOBJ_NAME, &THIS_MODULE->mkobj.kobj);
+
+	if (stats_kobj == NULL) {
+		DMERR("Failed to create kobject");
+		return -ENOMEM;
+	}
+
+	if (sysfs_create_file(stats_kobj, &stats_attr.attr)) {
+		DMERR("Failed to create stats attribute");
+		kobject_put(stats_kobj);
+		return -ENOMEM;
+	}
+
+	int result = dm_register_target(&dmpt);
+	if (result) {
+		DMERR("Failed to register target");
+		sysfs_remove_file(stats_kobj, &stats_attr.attr);
+		kobject_put(stats_kobj);
+	}
+
+	return result;
+}
+
+/*
+ * Unregister the target and remove sysfs on exit
+ */
+static void __exit proxy_exit(void) {
+	sysfs_remove_file(stats_kobj, &stats_attr.attr);
+	kobject_put(stats_kobj);
+	dm_unregister_target(&dmpt);
+}
+
+module_init(proxy_init);
+module_exit(proxy_exit);
 
 MODULE_AUTHOR("Alexander Bugaev");
-MODULE_DESCRIPTION(DM_NAME " target collecting request statistics");
+MODULE_DESCRIPTION("Proxy target collecting request statistics");
 MODULE_LICENSE("GPL");
 
